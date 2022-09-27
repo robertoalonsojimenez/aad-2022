6c4b27ea973fe398f0309fefdba1bf7cfff94e87
Roberto Alonso <roberto.alonso.jimenez.99@gmail.com>
2022-09-27 20:36:09 +0200

    Start project

--- app/src/main/java/com/ralonsoj/aad_2022/ut01/ex01/domain/Customer.kt
+++ app/src/main/java/com/ralonsoj/aad_2022/ut01/ex01/domain/Customer.kt
@@ -1,3 +1,3 @@
-package com.ralonsoj.aad_2022.ut01.domain
+package com.ralonsoj.aad_2022.ut01.ex01.domain
 
 data class Customer(val id: Int, val name: String, val surname: String, val isActive: Boolean)
